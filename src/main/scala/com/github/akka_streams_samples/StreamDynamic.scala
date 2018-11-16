82f3d0d977fb5e2a72ce6088fa6cc6bafb66fbc9
Mohamed ELHOSNI <mohamed.elhosni.exteam@axabanque.fr>
2018-11-16 11:05:00 +0100

    02 - Streamd Dynamic : Workign with MergeHub and BroadcastHub

--- src/main/scala/com/github/akka_streams_samples/StreamDynamic.scala
+++ src/main/scala/com/github/akka_streams_samples/StreamDynamic.scala
@@ -1,10 +1,12 @@
 package com.github.akka_streams_samples
 
-import akka.actor.ActorSystem
+import akka.{Done, NotUsed}
+import akka.actor.{ActorSystem, Cancellable}
+import akka.io.Udp.SO.Broadcast
 import akka.stream._
-import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.scaladsl.{BroadcastHub, Keep, MergeHub, RunnableGraph, Sink, Source}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 object StreamDynamic extends App {
 
@@ -57,6 +59,47 @@ object StreamDynamic extends App {
   })
   lastDelayed.onComplete(d => {
     print(s"shared-kill-switch delayed :${d}")
-    system.terminate()
+//    system.terminate()
   })
+
+  /**
+    * *****Dynamic fan-in and fan-out **********
+    */
+
+  /**
+    * 01 - Using the MergeHub
+    */
+  // prepare consumer
+  val consumer: Sink[String, Future[Done]] = Sink.foreach[String](println)
+  //  prepare th graph : attach the source of MH to consumer
+  val consumerDynamicGraph: RunnableGraph[Sink[String, NotUsed]] = MergeHub.source[String](perProducerBufferSize = 16).to(consumer)
+  // run/materialize the graph
+  val toConsumer: Sink[String, NotUsed] = consumerDynamicGraph.run()
+  // attach dynamic producer
+  println("01 - Using the MergeHub")
+  Source('A' to 'M').map(_.toString).toMat(toConsumer)(Keep.right)
+    .run()
+  Source('N' to 'Z').map(_.toString).toMat(toConsumer)(Keep.right)
+    .run()
+
+  /**
+    * 02 - Using the BroadcastHub
+    */
+  // prepare producer
+  val producer: Source[String, Cancellable] = Source.tick(1.seconds, 1.seconds, "Msg")
+  //  prepare th graph : attach the BH to the producer
+  val producerDynamicGraph: RunnableGraph[Source[String, NotUsed]] = producer.toMat(BroadcastHub.sink[String](bufferSize = math.pow(2,0).toInt))(Keep.right)
+  // run/materialize the graph
+  val fromProducer: Source[String, NotUsed] = producerDynamicGraph.run()
+  // attach dynamic consumer
+  println("02 - Using the BroadcastHub")
+  fromProducer.zipWith(Source(1 to 10))(Keep.both)
+    .toMat(Sink.foreach(println))(Keep.right)
+    .run()
+  fromProducer.zipWith(Source(11 to 20))(Keep.both)
+//    .delay(10.seconds, OverflowStrategy.dropHead)
+    .toMat(Sink.foreach(println))(Keep.right)
+    .run()
+    .onComplete(_ => system.terminate)
+
 }
